c5b5a277e6740221c5d852b1715030e5f958bd23
aacostas25 <aacostas@unal.edu.co>
2023-05-31 17:35:25 -0500

    Todo menos el playground

---
+++ App/redes.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.io.formats.info import Iterator
+from pydataset import data
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+#from sklearn.preprocessing import MinMaxScaler
+#from sklearn.datasets import make_blobs
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import io
+
+#Seleccionamos la base datos que queremos trabajar
+def seleccionarDB(numero):
+    'opciones para database: titanic, iris,snails,datos sinteticos'
+    if numero == 1:
+        baseDatos = data('titanic')
+        baseDatos = pd.get_dummies(baseDatos,drop_first=True)
+        X_train,x_test,Y_train,y_test = train_test_split(baseDatos.drop('survived_yes',axis = 1,), baseDatos['survived_yes'].values.reshape(-1,1),random_state=42)
+        x_inicial = 4
+    if numero == 2:
+        baseDatos = data('iris')
+        encoder = LabelEncoder()
+        baseDatos['Species'] = encoder.fit_transform(baseDatos['Species'])
+        X = baseDatos[['Sepal.Length','Sepal.Width','Petal.Length','Petal.Width']]
+        Y = baseDatos[['Species']]
+        X_train, x_test, Y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        x_inicial = 4
+
+    if numero == 3:
+        baseDatos = data('snails')
+        prueba = baseDatos['Deaths']/baseDatos['N']
+        baseDatos['Porcentaje_muertes'] = prueba
+        baseDatos = pd.get_dummies(baseDatos,drop_first=True)
+        X = baseDatos[['Species_B','Exposure','Rel.Hum','Temp']]
+        Y = baseDatos[['Porcentaje_muertes']]
+        X_train, x_test, Y_train, y_test = train_test_split(X, Y , test_size=0.2, random_state=42)
+        x_inicial = 4
+    return x_inicial,X_train,Y_train,x_test,y_test
+# Funciones de activacion
+
+def nonlin(x,deriv=False):
+    if(deriv==True):
+        return nonlin(x)*(1-nonlin(x))
+    return 1/(1+np.exp(-x))
+
+def tanh(x,deriv=False):
+  if(deriv==True):
+    return (1 - np.tanh(x)**2)
+  return np.tanh(x)
+
+#Seleccionamos la funcion de activacion
+
+def seleccionarFA(numero):
+    if numero == 1:
+        funcion = nonlin
+    if numero == 2:
+        funcion = tanh
+    return funcion
+"""
+Ahora definimos quien va a ser tanto el X,Y
+"""
+
+#Red neuronal con arquitectura x-10-7-1
+
+def RN2(X,Y,x_inicial,funcion = tanh,eta=0.01,neuronas_1 = 10,neuronas_2 = 7,ite = 3000):
+  np.random.seed(1)
+
+  # Inicializar los pesos de las capas ocultas y de la capa de salida
+  syn0 = 2*np.random.random((x_inicial,neuronas_1)) - 1
+  syn1 = 2*np.random.random((neuronas_1,neuronas_2)) - 1
+  syn2 = 2*np.random.random((neuronas_2, 1)) - 1
+
+  # Inicializar la lista de errores
+  errors = []
+  iteraciones = []
+  # Entrenar la red neuronal
+  for iter in range(ite):
+      # Propagación hacia adelante
+      l0 = X
+      neta1 = np.dot(l0, syn0)
+      l1 = funcion(neta1)
+      neta2 = np.dot(l1, syn1)
+      l2 = funcion(neta2)
+      neta3 = np.dot(l2, syn2)
+      l3 = funcion(neta3)
+
+      # Calcular el error
+      l3_error = Y - l3
+
+      if (iter % 100 == 0):
+        iteraciones.append(iter)
+        errors.append(np.mean(np.abs(l3_error), axis=0))
+
+      # Retropropagación del error
+      l3_delta = l3_error * funcion(neta3,deriv=True) * eta
+      l2_error = l3_delta.dot(syn2.T)
+      l2_delta = l2_error * funcion(neta2,deriv=True) * eta
+      l1_error = l2_delta.dot(syn1.T)
+      l1_delta = l1_error * funcion(neta1,deriv=True) * eta
+
+      # Actualizar los pesos
+      syn2 += np.dot(l2.T, l3_delta)
+      syn1 += np.dot(l1.T, l2_delta)
+      syn0 += np.dot(l0.T, l1_delta)
+
+      # Almacenar el error en la lista de errores
+  return (syn0,syn1,syn2,errors,iteraciones,l3_error)
--- App/routes.py
+++ App/routes.py
@@ -9,8 +9,8 @@ import plotly.express as px
 import csv
 import io
 import sys
-sys.path.append('/Users/andreacamilaacosta/ProyectoSIC/App')
-from otros import *
+sys.path.append('/Users/andreacamilaacosta/Proyecto-SIC/App/')
+from redes import *
 matplotlib.use('agg')
 
 @app.route("/")
@@ -29,33 +29,53 @@ def implementation():
     # Primero se deben capturar los parametros (dataset, algoritmo y valor de k)
     return render_template('implementation.html')
 def save_to_csv(data,perdida):
-    data.insert(0, perdida)  # ingresar el valor de funcion de perdida
+    data.insert(len(data), perdida)  # ingresar el valor de funcion de perdida
     with open('registros.csv', 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(data)
 
-@app.route('/Resultados',  methods=['GET'])
+@app.route('/Sinteticos',  methods=['GET', 'POST'])
+def sinteticos():
+    global valuesS
+    valuesS = [x for x in request.form.values()]
+    print(valuesS)
+    return render_template('sinteticos.html')
+
+@app.route('/results',  methods=['GET'])
 def results():
     try:
         print('lista: ', values)
-        l = int(values[2])
-        k = int(values[1])
-        d = int(values[4])
-        n = int(values[3])
         data_type = int(values[0])
+        iteraciones = int(values[1])
+        eta = float(values[2])
+        funciones = int(values[3])
+        neuronas1 = int(values[4])
+        neuronas2 = int(values[5])
         print(data_type)
-        x,y = cuadrado(n) #Funcion del archivo otros.py
-        save_to_csv(values,x[0])
+        x,X,Y,X_test,Y_test = seleccionarDB(data_type)
+        funcion = seleccionarFA(funciones)
+        capa0_1,capa1_1,capa2_1,errores_1,iteraciones,l3_error_1 = RN2(X,Y,x,funcion,eta = eta)
+        capa0_2,capa1_2,capa2_2,errores_2,iteraciones,l3_error_2 = RN2(X,Y,x,funcion,eta = eta+0.005)
+        capa0_3,capa1_3,capa2_3,errores_3,iteraciones,l3_error_3 = RN2(X,Y,x,funcion,eta = eta-0.005)
+        loss, = np.mean(np.abs(l3_error_1),axis=0)
+        save_to_csv(values[:-1],loss)
         plt.figure()
-        plt.plot(x,y)
+        plt.title('Error Red Neuronal')
+        plt.plot(iteraciones,errores_1)
+        plt.plot(iteraciones,errores_2)
+        plt.plot(iteraciones,errores_3)
+        plt.xlabel('Iteraciones')
+        plt.ylabel('Error medio')
+        plt.legend(['eta = '+str(0.01),'eta = '+str(0.015),'eta = '+str(0.005)])
+        #plt.show()
         plt.savefig(f"./App/static/images/result_random.png", transparent = True)
         plt.clf()
-        return render_template('results.html')
+        return render_template('results.html',loss=np.round(loss,3))
     except:
         return render_template('errores.html')
 
 @app.route('/Registro',methods=['GET'])
 def registro():
     df = pd.read_csv('registros.csv')
-    df_html = df.to_html()
+    df_html = df.to_html(classes='table table-striped')
     return render_template('registro.html',tabla = df_html)
